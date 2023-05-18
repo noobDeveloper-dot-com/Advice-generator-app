fc3d1b20ebbda12f1c49d66563e9fd097c237124
Abdul <textabdulhere@gmail.com>
2023-05-18 17:17:08 +0530

    Implemented Promises api

--- src/buttonAnimation.js
+++ src/buttonAnimation.js
@@ -4,7 +4,11 @@ export default function diceLoadingAnimation(is_dataLoaded) {
   const DICE_ICON = GENERATOR_BUTTON.querySelector("img");
   DICE_ICON.classList.add("animate-spin");
 
-  if (is_dataLoaded) {
+  if (is_dataLoaded === "completed") {
     setTimeout(() => DICE_ICON.classList.remove("animate-spin"), 500);
+  } else if (is_dataLoaded === "error") {
+    DICE_ICON.classList.remove("animate-spin");
+    DICE_ICON.classList.add("animate-pulse");
+    setTimeout(() => DICE_ICON.classList.remove("animate-pulse"), 1000);
   }
 }
--- src/main.js
+++ src/main.js
@@ -12,26 +12,39 @@ export default GENERATOR_BUTTON;
 GENERATOR_BUTTON.addEventListener("click", generateQuote);
 
 //functions
-
 function generateQuote() {
-  const URL = "https://type.fit/api/quotes";
-  const request = new XMLHttpRequest();
-
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState === 3) {
-      diceLoadingAnimation();
-    } else if (request.readyState === 4 && request.status === 200) {
-      //Converts json data into objects
-      const parseDATA = JSON.parse(request.responseText);
+  fetchDATA()
+    .then((parseDATA) => {
       // Generate random number between 0 - 1643
       const INDEX_NO = Math.round(Math.random() * 1643);
       const QUOTES_obj = parseDATA[INDEX_NO];
 
       DISPLAY_QUOTES.innerText = `"${QUOTES_obj.text}"`;
       QUOTE_NO.innerText = INDEX_NO;
-      diceLoadingAnimation("disable");
-    }
+      diceLoadingAnimation("completed");
+    })
+    .catch((error) => {
+      DISPLAY_QUOTES.innerText = `"${error}"`;
+    });
+}
+
+function fetchDATA() {
+  return new Promise((resolve, reject) => {
+    const URL = "https://type.fit/api/quotes";
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 3) {
+        console.log(request.status, request);
+        diceLoadingAnimation();
+      } else if (request.readyState === 4 && request.status === 200) {
+        //Converts json data into objects and pass it to resolve
+        resolve(JSON.parse(request.responseText));
+      } else if (request.readyState === 4) {
+        reject("Sorry no quotes atm. Try again later my friend :)");
+      }
+    });
+    request.open("GET", URL);
+    request.send();
   });
-  request.open("GET", URL);
-  request.send();
 }
